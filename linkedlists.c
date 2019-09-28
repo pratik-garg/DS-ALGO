b6f02eb2867d03a6d0d494bb6ad78ab2a29c4487
PRATIK GARG <golud.pg@gmail.com>
2019-09-28 19:23:44 +0530

    linked lists basic operations

--- linkedlists.c
+++ linkedlists.c
@@ -9,15 +9,36 @@ struct node
 typedef struct node * NODE;                        //using typedef
 NODE getnode();
 NODE insert_front(NODE );
+NODE delet_front(NODE);
+void display(NODE);
+NODE insert_rear(NODE);
+NODE delet_rear(first);
 int main()
 {
     NODE first;
-    first = NULL;                                  //initializing as NULL POINTER
+    first = NULL;
+    printf("INSERT ELEMENT\n");
+                                    //initializing as NULL POINTER
+    first = insert_front(first);
+    printf("INNSERET ELEMENT FORM FRONT END TWO TIMES\n");
     first = insert_front(first);
     first = insert_front(first);
-    first = insert_front(first);
-    printf("%d->%d->%d\n",(first)->info,((first)->link)->info,((((first)->link)->link)->info));
+    printf("displaying the elements of the linked lists\n");
+    display(first);
+    printf("NOW TIME TO DELETE FROM FRONT\n");
+    first = delet_front(first);
+    printf("displaying the elements of the linked lists\n");
+    display(first);
             //PRINNTING IT IN LINKED LISTS PATTERN LIKE THIS info3->info2->info1.
+    printf("INSERT ELEMENTS FORM REAR END TWO TIMES\n");
+    first = insert_rear(first);
+    first = insert_rear(first);
+    printf("displaying the elements of the linked lists\n");
+    display(first);
+    printf("DELETING FROM REAR\n");
+    first = delet_rear(first);
+    printf("displaying the elements of the linked lists\n");
+    display(first);
 }
 NODE getnode()                                    //THIS WILL GENERATE STRUCTURE NODE
 {
@@ -42,4 +63,96 @@ NODE insert_front(NODE first)                       //THIS WILL CONNECT AND CREA
     (temp)->link = first;
     return temp;
 
+}
+void display(NODE first)
+{
+    NODE curr = first;
+    if(curr == NULL)
+    {
+        printf("NO ELEMENTS TO DISPLAY\n");
+        return ;
+    }
+    while(curr!=NULL)
+    {
+        printf("%d ",curr->info);
+        curr = curr->link;
+    }
+    printf("\n");
+}
+NODE delet_front(NODE first)
+{
+    NODE next;
+    if(first==NULL)
+    {
+        printf("NO ELEMENTS IN THE SINGLY LINKED LIST\n");
+        return ;
+    }
+
+    else if (first->link==NULL)
+    {
+        printf("ELEMENT DELETED\n");
+        return NULL;
+    }
+
+    else
+    {
+        next = first->link;
+        free(first);
+        return next;
+    }
+}
+NODE insert_rear(NODE first)
+{
+    NODE next=getnode();
+    printf("ENTER THE ELEMENT\n");
+    scanf("%d",&next->info);
+    next->link = NULL;
+    if(first == NULL)
+    {
+        return next;
+    }
+
+    else if(first->link ==NULL)
+    {
+        first->link = next;
+        return first;
+    }
+
+    else
+    {
+        NODE curr = first;
+        while(curr->link!=NULL)
+        {
+            curr = curr->link;
+        }
+        curr->link = next;
+    }
+    return first;
+}
+NODE  delet_rear(NODE first)
+{
+    NODE temp=first,curr,prev;
+    if(temp==NULL)
+    {
+        printf("NO ELEMENTS IN THE LIST\n");
+        return NULL;
+    }
+
+    else if(temp->link==NULL)
+    {
+        free(first);
+        return NULL;
+    }
+    else
+    {
+        curr = first;
+        while(curr->link!=NULL)
+        {
+            prev=curr;
+            curr = curr->link;
+        }
+        free(curr);
+        prev->link = NULL;
+        return first;
+    }
 }
